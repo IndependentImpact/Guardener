466b2f2d901893b69449b75ef2d1b946f8b13d52
christiaanpauw <christiaan.pauw@nova.org.za>
2023-02-12 12:29:44 +0200

    Add whichBlocksHasSchema

---
+++ R/whichBlocksHasSchema.R
@@ -0,0 +1,30 @@
+
+
+#' whichBlocksHasSchema
+#' @description Check in a collection of blocks which blocks are associated with a particular schema
+#' @param dfBlocks tibble. Typically the output of GgetPolicyBlocks()
+#' @param schemaId character. The uuid of a schema
+#' @param washfunction function or NULL. Function to clean the variable containing the schemaId (specified by schemaname)
+#' in dfBlocks. Default function(x){gsub("#", "", x)} . If NULL no cleaing is performed
+#' @param schemaname Character. Variable in dfBlocks in which to look for schemaId
+#' @return
+#' @export
+#'
+#' @examples b <- whichBlocksHasSchema(GgetPolicyBlocks(AT, policyId = policyid),  "a399cc58-9a79-4277-b5ef-1c6461843a58")
+
+whichBlocksHasSchema <- function(dfBlocks,
+                                 schemaId,
+                                 washfunction = c(function(x){gsub("#", "", x)}, NULL)[1],
+                                 schemaname = "schema"
+                                 ){
+  message(schemaId)
+  if (!is.data.frame(dfBlocks)) stop("dfBlocks must be a data frame")
+  if (!schemaname %in% colnames(dfBlocks)) stop(schemaname , " does not occur in dfBlocks")
+  if (!is.null(washfunction)) {dfBlocks <- dfBlocks %>% mutate(schema = washfunction[[1]](.[[!!schemaname]]))}
+
+  dfBlocks %>% filter(.[[!!schemaname]] %in% schemaId)
+
+
+
+
+}
