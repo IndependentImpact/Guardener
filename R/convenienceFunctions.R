6c862445bb1548f2010a55840c07c2680b72a7a3
christiaanpauw <christiaan.pauw@nova.org.za>
2023-02-22 11:36:34 +0200

    voeg ones() toe

--- R/convenienceFunctions.R
+++ R/convenienceFunctions.R
@@ -3,4 +3,15 @@ extractTopic <- function(policy){
   topicId <- policy$topicId
 }
 
+#' ones
+#'
+#' @param df
+#'
+#' @return character vector with names of df that contain nested items of length 1
+#' @export
+#'
+#' @examples df %>% unnest(cols = ones(.))
 
+ones <- function(df){
+  names(df)[map_lgl(df, ~all(map_int(., length) == 1))]
+}
