f4f32dcb43371ecfce70bf56e47ba8bbf84d97af
christiaanpauw <christiaan.pauw@nova.org.za>
2023-02-22 11:02:36 +0200

    herskryf Glist2tibble

--- R/Glist2tibble.R
+++ R/Glist2tibble.R
@@ -1,42 +1,39 @@
+
+
 #' Glist2tibble
 #'
 #' @param L list
-#' @param flatfirst logical
+#'
 #' @return tibble
-#' @importFrom dplyr %>%
 #' @export
 
-Glist2tibble <- function(L, flatfirst = FALSE){
-  if (flatfirst){
-    df <- map_df(L, ~{
-    Lnames <- flatten(.)  %>% names()
-    Lf <- flatten(.)
-    if (any(map_lgl(Lf, ~is.null(.))))   Lf[map_lgl(Lf, ~is.null(.))] <- NA_character_
-    if (any(map_lgl(Lf, ~length(.)==0))) Lf[map_lgl(Lf, ~length(.)==0)]  <- NA_character_
-    names(Lf) <- Lnames
-    df <- Lf %>%
-      tibble() %>%
-      pivot_longer(everything()) %>%
-      mutate(name = Lnames) %>%
-      #unnest(value) %>%
-      pivot_wider(names_from = name, values_fn = NULL)
-    nn <- names(df)[map_lgl(df, ~length(.[[1]]) > 1)]
-    df %>% unnest(cols = -{{nn}})
-    })
+Glist2tibble <- function(L, verbose = FALSE){
+  if (any(map_lgl(L, ~is.null(.))))   L[map_lgl(L, ~is.null(.))] <- NA_character_
+  if (any(map_lgl(L, ~length(.)==0))) L[map_lgl(L, ~length(.)==0)]  <- NA_character_
 
-  } else {
+  if (verbose) assign("LL", L, envir = .GlobalEnv)
+
+  if (length(L) == 1) L <- L[[1]]
+
+  if (is.null(names(L))) {
     df <- map_df(L, ~{
-    Lnames <- names(.)
-    if (any(map_lgl(., ~is.null(.)))) .[map_lgl(., ~is.null(.))] <- NA_character_
-    if (any(map_lgl(., ~length(.)==0))) .[map_lgl(., ~length(.)==0)]  <- NA_character_
-    names(.) <- Lnames
-    df <- tibble(.) %>%
-      pivot_longer(everything()) %>%
-      mutate(name = Lnames) %>%
-      pivot_wider(names_from = name, values_fn = NULL)
-    nn <- names(df)[map_lgl(df, ~length(.[[1]]) > 1)]
-    df %>% unnest(cols = -{{nn}})
-    })
+      n <- names(.)
+      as_tibble_col(.) %>%
+        mutate(names = n) %>%
+        pivot_wider(names_from = "names")}
+    )
+  } else {
+    df <- as_tibble_col(L) %>%
+      mutate(names = names(L)) %>%
+      pivot_wider(names_from = "names")
   }
-  df
+
+  if (verbose) assign("dff", df, envir = .GlobalEnv)
+
+  nn <- names(df)[map_lgl(df, ~length(.[[1]]) > 1| is.null(.[[1]]) | length(.[[1]]) == 0 )]
+  if (length(nn) == 0) {
+    return(df)
+  }
+  df %>% unnest(cols = -{{nn}})
+
 }
