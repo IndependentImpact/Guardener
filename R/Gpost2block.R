2f4a9f0da8395185767a91a77ecbd957e3c9c3f7
AlexIvanHoward <alexivanhoward@gmail.com>
2024-01-26 21:37:02 +0200

    Handles result returned by Guardian after post to block

--- R/Gpost2block.R
+++ R/Gpost2block.R
@@ -2,21 +2,19 @@
 #' Gpost2block
 #'
 #' @param refreshToken Character. JWT refresh token returned by Glogin()$refreshToken.
-#' @param document Named list or data frame with data to submit. Names must correspond to field names in Schema.
-#' @param schemaID character. Schema to submit to.
-#' @param schemaIPFSurl character. Schema to submit to.
+#' @param x List. Data to submit to the policy block. If x is a document based
+#'   on a schema, the document should already contain appropriately populated
+#'   'context' and 'type' fields.
 #' @param policyId character. Schema to submit to.
-#' @param BlockId character. Schema to submit to.
+#' @param blockId character. Schema to submit to.
 #' @param baseurl Character. URL where /api/ is hosted. Default "http://localhost:3000/".
-#' @return NULL. #TODO. This is not accurate. It does return stuff.
+#' @return List. Some metadata about the submission.
 #' @export
 
-Gpost2block <- function(refreshToken,
-                        document,
-                        schemaID,
-                        schemaIPFSurl,
+Gpost2block <- function(x,
+                        refreshToken,
                         policyId,
-                        BlockId,
+                        blockId,
                         baseurl = "http://localhost:3000/"){
 
   # Get access token for this query.
@@ -24,10 +22,24 @@ Gpost2block <- function(refreshToken,
                                  baseurl = baseurl)
 
   # Make the query.
-  httr::POST(
-    url = sprintf("%sapi/v1/policies/%s/blocks/%s", baseurl, policyId, BlockId),
+  res <- httr::POST(
+    url = sprintf("%sapi/v1/policies/%s/blocks/%s", baseurl, policyId, blockId),
     httr::add_headers(Authorization = sprintf("Bearer %s", accessToken)),
-    body = GmakeBody(document, schemaID, schemaIPFSurl),
+    body = x,
     encode = "json")
+
+  if (!res$status_code == 200) {
+    stop(sprintf("ERROR. Failed to post to block. Status code: %s",
+                 res$status_code))
+  }
+
+  res <- httr::content(res, as = "parsed")
+
+  return(res)
 }
+
+# "@context": [
+#   "ipfs://bafkreifadsohb4fomdgqrftlqdlzdx7qhar7nfxdy76bulcqpawghu4poy"
+# ],
+# "type": "9d164446-9596-497d-8990-3ceefa7b8a62&1.0.0"
 
