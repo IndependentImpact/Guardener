e6598aedbc66dd87db87f3dd505e8eafe9289509
AlexIvanHoward <alexivanhoward@gmail.com>
2025-01-21 01:05:02 +0400

    Work around Guardian API bug.

--- R/GgetSchema.R
+++ R/GgetSchema.R
@@ -29,6 +29,18 @@ GgetSchema <- function(refreshToken,
   # Process the result.
   res <- httr::content(res)
 
+  # Due to a bug in the Guardian API, the '$defs' field is empty in the version of the
+  # schema definition included in the 'document' list item, so we need to replace that
+  # item with the full definition retrieved from IPFS.
+  ipfsUrl <- res$documentURL
+  if (length(grep(pattern = "http", x = ipfsUrl, value = FALSE)) == 0) {
+    ipfsUrl <- gsub(pattern = "ipfs://", replacement = "", x = ipfsUrl, fixed = TRUE)
+    ipfsUrl <- sprintf("https://%s.ipfs.w3s.link", ipfsUrl)
+  }
+  doc <- httr::GET(url = ipfsUrl)
+  doc <- content(x = doc, as = "parsed")
+  res$document <- jsonlite::toJSON(x = doc, auto_unbox = TRUE)
+
   if (returndf){
     res <- res[which(sapply(X = res, FUN = function(x) length(x) > 0))]
     res <- data.frame(res)
